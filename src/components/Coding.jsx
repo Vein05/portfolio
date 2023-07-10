ffd457ea94939d8025c2e32fffed29a794a5e99b
Vein05 <72024472+Vein05@users.noreply.github.com>
2023-07-10 18:08:42 +0545

    Created : Coding for 2nd page of the website
    Will show stats, and languages I use.

---
+++ src/components/Coding.jsx
@@ -0,0 +1,19 @@
+function Coding(){
+    return(
+        <div className="pt-10 flex space-x-5 justify-center items-center">
+            <img
+            src="https://github-readme-stats.vercel.app/api?username=vein05&show_icons=true&theme=dracula&card=600&line_height=30&include_all_commits=true"
+            alt="Stats"/>
+            
+            <img
+            src="https://github-readme-stats.vercel.app/api/top-langs/?username=vein05"
+            alt="stats"
+            />
+        </div>
+
+    )
+
+}
+
+
+export default Coding
