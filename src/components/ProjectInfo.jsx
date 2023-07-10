0cffaaf1b15692416a00038f2d86e8ce4739bf10
Vein05 <72024472+Vein05@users.noreply.github.com>
2023-07-10 21:31:10 +0545

    Updated: Added in design changes

--- src/components/ProjectInfo.jsx
+++ src/components/ProjectInfo.jsx
@@ -8,12 +8,12 @@ import {
 import Image from "../images/image.jpg"   
   export default function Example() {
     return (
-      <Card color="transparent" shadow={false} className="w-full max-w-[26rem]">
+      <Card color="transparent" shadow={false} className="w-full max-w-[26rem] p-2 border-2 border-dashed border-blue-500 rounded">
         <CardHeader
           color="transparent"
           floated={false}
           shadow={false}
-          className="mx-0 flex items-center gap-4 pt-0 pb-8"
+          className="mx-0 flex items-center justify-center gap-4 pt-0 pb-8"
         >
           <Avatar
             size="lg"
@@ -26,12 +26,12 @@ import Image from "../images/image.jpg"
                 Sugam Panthi
               </Typography>
             </div>
-            <Typography color="blue-gray">Self-Taught Developer [Python, WebDev]</Typography>
+            <Typography color="blue-gray">@Self-Taught Developer </Typography>
           </div>
         </CardHeader>
         <CardBody className="mb-6 p-0">
           <Typography>
-            &quot;Throughout my personal odyssey in the realm of computer programming and software engineering, I have reached some significant pinnacles. These include but are not limited to: the production of a highly-regarded Discord bot that has amassed over a million users, the creation of a user interface for a scientific calculator that is both intuitive and user-friendly, the development of a comprehensive music player for Linux operating systems, participation in a prestigious Discord bot project, and the construction of a robust web application using React, Python, and Firebase. These accomplishments are a testament to my ardor for programming and software development, and my unwavering devotion to delivering practical solutions that are imbued with an amiable and congenial touch.&quot;
+          &nbsp; &nbsp; &quot;Throughout my personal odyssey in the realm of computer programming and software engineering, I have reached some significant pinnacles. These include but are not limited to: the production of a highly-regarded Discord bot that has amassed over a million users, the creation of a user interface for a scientific calculator that is both intuitive and user-friendly, the development of a comprehensive music player for Linux operating systems, participation in a prestigious Discord bot project, and the construction of a robust web application using React, Python, and Firebase. These accomplishments are a testament to my ardor for programming and software development, and my unwavering devotion to delivering practical solutions that are imbued with an amiable and congenial touch.&quot;
           </Typography>
         </CardBody>
       </Card>
