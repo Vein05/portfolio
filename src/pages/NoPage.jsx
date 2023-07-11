09f6ac96463696abf5ca84675add6b2ce75251d0
Vein05 <72024472+Vein05@users.noreply.github.com>
2023-07-11 11:31:19 +0545

    Created: 404 page

---
+++ src/pages/NoPage.jsx
@@ -0,0 +1,18 @@
+import Nav from "../components/Nav"
+
+function NoPage(){
+    return (
+        <>
+        <Nav/>
+            <div>
+
+                <h1 className="text-center font-custom text-5xl">THIS PAGE DOESN"T EXIST!</h1>
+
+
+            </div>
+        </>
+    )
+}
+
+
+export default NoPage
