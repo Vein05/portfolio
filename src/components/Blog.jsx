2106fba69b638b2db3f5d4d3b2d0c1d5e565f786
Vein05 <72024472+Vein05@users.noreply.github.com>
2023-07-11 11:31:08 +0545

    Misc Changes

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -3,18 +3,22 @@ import BlogCard from "./blogFiles/BlogCard";
 
 function Blog(){
     return (
-        <div id="Blog" className="pt-8 flex justify-center items-center space-x-5">
-            <BlogCard 
-            title="10 Must know Tips for the PTE examination"
-            description= "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam minus magni maiores autem eius dolorem, obcaecati error repellendus veritatis quasi iste voluptatum. Porro et deleniti maxime ex ducimus nulla eligendi!"
-            image = "./images/pte.jpg"
-            
-            />
-            <BlogCard
-            title="Best FREE resources to Ace the Digital-SAT" 
-            description= "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam minus magni maiores autem eius dolorem, obcaecati error repellendus veritatis quasi iste voluptatum. Porro et deleniti maxime ex ducimus nulla eligendi!"
-            image = "./images/SAT.svg"
-            />
+        <div className="flex flex-col space-x-6 justify-center items-center">
+            <p className="font-custom text-6xl">Blogs & Articles</p>
+            <div id="Blog" className="pt-8 flex justify-center items-center space-x-5">
+                <BlogCard 
+                title="10 Must know Tips for the PTE examination"
+                description= "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam minus magni maiores autem eius dolorem, obcaecati error repellendus veritatis quasi iste voluptatum. Porro et deleniti maxime ex ducimus nulla eligendi!"
+                image = "/images/pte.jpg"
+                page = "/blogs/10-best-tips-for-the-PTE-examination"
+                />
+                <BlogCard
+                title="Best FREE resources to Ace the Digital-SAT" 
+                description= "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam minus magni maiores autem eius dolorem, obcaecati error repellendus veritatis quasi iste voluptatum. Porro et deleniti maxime ex ducimus nulla eligendi!"
+                image = "/images/SAT.svg"
+                page="/blogs/best-free-resources-for-the-digital-SAT"
+                />
+            </div>
         </div>
     )
 }
