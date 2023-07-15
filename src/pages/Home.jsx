bd6477f48b752a92450320a2218acaf7d7586580
Vein05 <72024472+Vein05@users.noreply.github.com>
2023-07-15 12:10:23 +0545

    Re-did the animations

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useRef } from 'react';
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import React, { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
 import Intro from "../components/Intro";
@@ -10,7 +10,6 @@ import Blog from "../components/Blog";
 import Coding from "../components/Coding";
 import Contact from "../components/Contact";
 
-
 gsap.registerPlugin(ScrollTrigger);
 
 function Home() {
@@ -19,94 +18,86 @@ function Home() {
   useEffect(() => {
     const element = ref.current;
     gsap.fromTo(
-      element.querySelector('.intro'), 
-      { autoAlpha: 0, x: -200 }, 
-      { 
-        duration: 1.5,  
-        autoAlpha: 1,  
+      element.querySelector(".intro"),
+      { autoAlpha: 0, x: -200 },
+      {
+        duration: 1.5,
+        autoAlpha: 1,
         x: 0,
-        ease: "power3.out" 
-      });
-      // gsap.fromTo(
-      //   element.querySelector('.github'), 
-      //   { autoAlpha: 0, x: 1200 }, 
-      //   {
-      //     duration: 1.5,  
-      //     autoAlpha: 1,  
-      //     x: 0, 
-      //     ease: "power3.out",
-      //     scrollTrigger: {
-      //       trigger: '.github',
-      //       start: 'top center',
-      //       end: 'bottom center',
-      //       scrub: true
-      //     }})
+        ease: "power3.out",
+      }
+    );
+    // gsap.fromTo(
+    //   element.querySelector('.github'),
+    //   { autoAlpha: 0, x: 1200 },
+    //   {
+    //     duration: 1.5,
+    //     autoAlpha: 1,
+    //     x: 0,
+    //     ease: "power3.out",
+    //     scrollTrigger: {
+    //       trigger: '.github',
+    //       start: 'top center',
+    //       end: 'bottom center',
+    //       scrub: true
+    //     }})
 
     gsap.fromTo(
-      element.querySelector('.coding'), 
-      { autoAlpha: 0, x: -200 }, 
+      element.querySelector(".coding"),
+      { autoAlpha: 0, x: -200 },
       {
-        duration: 2,  
-        autoAlpha: 1,  
-        x: 0, 
+        duration: 2,
+        autoAlpha: 1,
+        x: 0,
         ease: "power3.out",
         scrollTrigger: {
-          trigger: '.coding',
-          start: 'top center',
-          end: 'bottom center',
-          scrub: true
-        }})
-   }, []);
+          trigger: ".coding",
+          start: "top center",
+          end: "bottom center",
+          scrub: true,
+        },
+      }
+    );
+  }, []);
 
-    return(
-        <div className="bg-main" ref={ref}>
-            <div className=" p-2 flex space-x-8 flex-col min-h-screen">
-            <Nav/>
+    return (
+      <div className="bg-main" ref={ref}>
+        <div className="p-2 flex flex-col min-h-screen">
+          <Nav />
+          <div className="flex-1 space-y-5">
             <div className="intro">
-                <Intro/>
+              <Intro />
+              <Footer scrollTo=".coding" />
             </div>
-            <div className=" py-4 mt-auto">
-            <Footer/>
+
+            <div className="coding">
+              <Coding />
+              <Footer scrollTo=".longIntro" />
             </div>
+
+            <div className="longIntro">
+              <LongIntro />
+              <Footer scrollTo=".github" />
+            </div>
+
+            <div className="github">
+              <Github />
+              <Footer scrollTo=".blog" />
+            </div>
+
+            <div className="blog">
+              <Blog />
+              <Footer scrollTo=".contact" />
+            </div>
+
+            <div className="contact">
+              <Contact />
+            </div>
+          </div>
         </div>
-
-        <div className="coding p-2 flex flex-col min-h-screen">
-            <Coding/>
-            <div className=" py-4 mt-auto">
-            <Footer/>
-            </div>
-        </div>
-
-        <div className="longIntro pt-2 flex flex-col min-h-screen">
-            <LongIntro/>
-            <div className="pr-5 pl-5 mt-auto">
-            <Footer/>
-            </div>
-        </div>
-
-        <div className="github pt-2 flex flex-col min-h-screen space-y-5">
-            <Github />
-            <div className="pr-5 pl-5 mt-auto">
-            <Footer/>
-            </div>
-        </div>
-
-        <div className="blog pt-2 flex flex-col min-h-screen space-y-5">
-            <Blog/>
-            <div className="pr-5 pl-5 mt-auto">
-            <Footer/>
-         </div>
-
       </div>
+    );
+  }
 
-      <div className="contact pt-2 flex flex-col min-h-screen space-y-5">
-            <Contact/>
-        </div>
 
-    </div>
-
-    )
-
-}
-
-export default Home
+export default Home;
