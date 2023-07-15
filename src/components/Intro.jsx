451550bf90389615cce0d613e8285b41f8fcdfba
Vein05 <72024472+Vein05@users.noreply.github.com>
2023-07-15 12:09:49 +0545

    Misc changes

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,11 +3,11 @@ function Intro(){
     return(
         <div id="About"className="flex justify-center items-center flex-col space-y-10">
             <div className="flex justify-center items-center flex-col space-y-5">
-                <p className="m-10"><Quote/></p>
+                <span className="m-10"><Quote/></span>
                 <div className="flex justify-center items-center flex-col space-y-10">
                     <h1 className=" text-8xl" >Hello, <br/>I'm <u className="text-blue-500"> Sugam </u>
                     </h1>
-                    <p className="text-xl">I'm an 18 year old <span className="font-custom text-blue-500">Tech Aficionado </span> from <a target="_blank"  rel="noreferrer" href="https://en.wikipedia.org/wiki/Nepal">🇳🇵</a> who loves to code and develop new skills.</p>
+                    <span className="text-xl">I'm an 18 year old <span className="font-custom text-blue-500">Tech Aficionado </span> from <a target="_blank"  rel="noreferrer" href="https://en.wikipedia.org/wiki/Nepal">🇳🇵</a> who loves to code and develop new skills.</span>
 
                 </div>
             </div>
