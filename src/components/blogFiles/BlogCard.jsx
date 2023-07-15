227bbc56059f8ecbf59f50c2d8abe2c83e5a9886
Vein05 <72024472+Vein05@users.noreply.github.com>
2023-07-15 12:08:59 +0545

    Update: BIg changes, Stopped using Material UI

--- src/components/blogFiles/BlogCard.jsx
+++ src/components/blogFiles/BlogCard.jsx
@@ -1,69 +1,35 @@
-import {
-    Card,
-    CardHeader,
-    CardBody,
-    CardFooter,
-    Typography,
-    Avatar,
-    Tooltip,
-		Button,
-} from "@material-tailwind/react";
-
-import { ArrowLongRightIcon } from "@heroicons/react/24/outline";
-
-
-
-function BlogCard(props){
+function BlogCard(props) {
   const handelClick = () => {
     window.open(props.page, "_blank");
-  }
-    return (
-        <Card className="overflow-hidden"  style={{ width: '24rem' }}>
-          <CardHeader
-            floated={false}
-            shadow={false}
-            color="transparent"
-            className="m-0 rounded-none"
-          >
-          <a href={props.page} >
-            <img
-              src={props.image}
-              alt=""
-            />
-            </a>
-          </CardHeader>
-          <CardBody color="" className="color-main">
-            <Typography variant="h4" color="blue-gray">
-              {props.title}
-            </Typography>
-            <Typography variant="lead" color="gray" className="mt-3 font-normal">
-							{props.description}
-            </Typography>
-						<a href="#Blog" className="">
-							<Button variant="text" className="pt-3 flex items-center gap-2" onClick={handelClick}>
-								Learn More 
-								<ArrowLongRightIcon strokeWidth={2} className="w-4 h-4" />
-							</Button>
-        		</a>
-          </CardBody>
-          <CardFooter className="flex items-center justify-between">
-            <div className="flex items-center -space-x-3">
-              <Tooltip content="Sugam Panthi">
-                <Avatar
-                  size="sm"
-                  variant="circular"
-                  alt="sugam panthi"
-                  src = "/images/image.jpg"
-                  className="border-2 border-white hover:z-10"
-                />
-              </Tooltip>
-            </div>
-            <Typography className="font-normal">500 👁 | July 10</Typography>
+  };
+  return (
+    <div className="w-full sm:w-96 overflow-hidden flex-1">
+      <div className="m-0">
+        <a href="#Blog" onClick={handelClick}>
+          <img className="rounded-md bg-transparent" src={props.image} alt="" height={300}  />
+        </a>
+      </div>
+      <div className="p-4 color-main">
+        <h2 className="text-xl sm:text-2xl font-bold text-blue-500">{props.title}</h2>
+        <p className="mt-3 text-base sm:text-lg font-normal text-gray">➤ {props.description}</p>
+        <a href="#Blog" className="">
+          <button className="mt-3 flex items-center gap-2 text-blue-500 hover:text-blue-800 focus:outline-none">
+            Learn More
+          </button>
+        </a>
+      </div>
+      <div className="flex items-center justify-between p-4">
+        <div className="flex items-center space-x-2 sm:space-x-3">
+          <div className="flex items-center">
+            <img src="/images/image.jpg" alt="sugam panthi" className="w-6 h-6 sm:w-8 sm:h-8 rounded-full border-2 border-white hover:z-10" />
+            <span className="absolute bottom-0 right-0 inline-block w-2 h-2 sm:w-2.5 sm:h-2.5 border-2 border-white rounded-full"></span>
+          </div>
+          <p className="hidden sm:inline-block text-gray text-sm font-medium">Sugam Panthi</p>
+        </div>
+        <p className="text-gray text-sm font-normal">500 👁 | July 10</p>
+      </div>
+    </div>
+  );
+}
 
-          </CardFooter>
-        </Card>
-      );
-    }
-
-
-export default BlogCard
+export default BlogCard;
