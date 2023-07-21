a9dde8a4150510388afa5b31396f52a0b891a9b3
Vein05 <72024472+Vein05@users.noreply.github.com>
2023-07-21 19:35:41 +0545

    Re did this to make it responsive

--- src/components/RepoCard.jsx
+++ src/components/RepoCard.jsx
@@ -1,76 +1,58 @@
-import Abode from "../images/abode.png";
-import Calculator from "../images/calc.jpg";
-import Danime from "../images/danime.png";
-import QPlayer from "../images/qplayer.jpg";
-import Quotient from "../images/quotient.png";
-
 import AvaIcon from "./AvaIcon";
 import MyContribution from "./MyContribution";
 
-function RepoCard(props) {
-  const { stats } = props;
+import spicyInfoData from "../data/spicyData.json";
 
-  let image;
-  switch (stats.repo) {
-    case "Abode":
-      image = Abode;
-      break;
-    case "Calculator":
-      image = Calculator;
-      break;
-    case "Danime":
-      image = Danime;
-      break;
-    case "QPlayer":
-      image = QPlayer;
-      break;
-    case "Quotient-Bot":
-      image = Quotient;
-      break;
-    default:
-      break;
+function spicyInfo(repo) {
+  const repoInfo = spicyInfoData.find((data) => data.repo === repo);
+  if (repoInfo) {
+    return (
+      <ul className="pl-5 list-disc">
+        {repoInfo.info.map((info, index) => (
+          <li key={index}>{info}</li>
+        ))}
+      </ul>
+    );
+  } else {
+    return null;
   }
+}
 
-  function handleClick() {
-    window.open("https://github.com/" + stats.fullName, "_blank");
-  }
+
+function RepoCard({ stats, image, imagePosition }) {
+  const handleClick = () => {
+    window.open(`https://github.com/${stats.fullName}`, "_blank");
+  };
 
   return (
-    <div className="w-96 px-2 py-2 ">
-      <div className="overflow-hidden">
-        <div className="">
-          <img src={image} alt="" className="w-full h-full rounded-full object-cover" />
+    <div className="flex flex-col items-center space-y-5 pt-5">
+      <div className={`flex flex-col ${imagePosition === "left" ? "md:flex-row-reverse" : "md:flex-row"} items-center space-y-2 md:items-start md:space-x-5 w-full md:w-3/4 lg:w-2/3`}>
+        <div className="flex-shrink-0">
+          <img src={image} className="rounded-full object-cover h-96 w-96 md:h-72 md:w-72" alt="" />
         </div>
-        <div className="p-4 space-y-2">
-          <div className="flex items-center justify-between mb-2">
-            <h2 className="capitalize font-bold text-2xl text-blue-500">{stats.repo}</h2>
-            <p className="font-medium ">🌟 {stats.totalStars}</p>
-          </div>
-          <div className="flex flex-col space-y-2">
-            <p className="inline-flex">My Role: &nbsp;<MyContribution repo={stats.repo} /> </p>
-            <p className="inline-flex">Description:&nbsp;{stats.description}</p>
-            </div>
+        <div className="space-y-2 text-center md:text-left">
+          <h2 className="text-xl text-center md:text-2xl font-bold text-blue-500 capitalize">{stats.repo}</h2>
+          <span className="text-sm md:text-xl">Stars : {stats.totalStars}</span>          
+          <p className="text-sm md:text-xl">Description: {stats.description}</p>
+          <p className="text-sm md:text-xl inline-flex">
+            My Role : <MyContribution repo={stats.repo} />
+          </p>
+          <p className="text-sm md:text-xl">Spicy info : {spicyInfo(stats.repo)}</p>
         </div>
-        <div className="p-4 pt-0">
-          <div className="flex flex-col space-y-4">
-            <div className="flex flex-wrap items-center justify-center space-x-2">
-              <span>Devs: </span>
-              {stats.contributors.map((contributor, index) => (
-                <AvaIcon
-                  key={index}
-                  name={contributor.name}
-                  link={contributor.avatarUrl}
-                />
-              ))}
-            </div>
-            <button
-              className="text-center py-2 block w-full bg-blue-gray-900/10 text-blue-gray-900 hover:bg-blue-gray-900/20 font-bold rounded-md transition duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue"
-              onClick={handleClick}
-            >
-              Visit on GitHub
-            </button>
-          </div>
+      </div>
+      <div className="space-y-4 text-center md:text-left w-full md:w-3/4 lg:w-2/3">
+        <div className="flex flex-wrap items-center justify-center space-x-2">
+          <span className="text-sm md:text-base">Devs:</span>
+          {stats.contributors.map((contributor, index) => (
+            <AvaIcon key={index} name={contributor.name} link={contributor.avatarUrl} />
+          ))}
         </div>
+        <button
+          className="block w-full py-2 font-bold text-blue-gray-900 bg-blue-gray-900/10 hover:bg-blue-gray-900/20 rounded-md transition duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue"
+          onClick={handleClick}
+        >
+          Visit on GitHub
+        </button>
       </div>
     </div>
   );
