bf76c0c0e97f74197c2a01850868dfc2be06aa67
Vein05 <72024472+Vein05@users.noreply.github.com>
2023-07-21 19:36:06 +0545

    Redid the UI and made it responsive

--- src/components/Github.jsx
+++ src/components/Github.jsx
@@ -1,46 +1,73 @@
-import Repo from "./Repo";
-import ProjectInfo from "./ProjectInfo";
-import {useState} from "react";
-import {Button} from "@material-tailwind/react"
+import { useEffect, useState } from "react";
+import { getRepositoryStats } from "./github";
+import AbodeImage from "../images/abode.png";
+import CalculatorImage from "../images/calc.jpg";
+import DanimeImage from "../images/danime.png";
+import QPlayerImage from "../images/qplayer.jpg";
+import QuotientImage from "../images/quotient.png";
+import RepoCard from "./RepoCard";
+
+const repoImages = {
+  Abode: AbodeImage,
+  Calculator: CalculatorImage,
+  Danime: DanimeImage,
+  QPlayer: QPlayerImage,
+  "Quotient-Bot": QuotientImage,
+};
+
+const repos = ["Danime", "Quotient-Bot", "QPlayer", "Abode", "Calculator"];
 
 function Github() {
-  const repos = ["Danime", "Quotient-Bot", "QPlayer", "Abode",  "Calculator" ];
+  const [repoStats, setRepoStats] = useState(null);
   const [currentRepoIndex, setCurrentRepoIndex] = useState(0);
 
-    return (
-      <div id="Projects">
-        <h1 className="bold text-center underline text-3xl">Projects I've worked on</h1>
-        <div className="flex pl-10 pt-5 space-x-32 items-center justify-center">
-          <ProjectInfo/>
-          <div className="pt-5 flex flex-col justify-center items-center space-y-5">
-            
-            
-            <div className="flex flex-row items-center justify-center space-x-5">
-              
-              <Button
-                color="light-blue"
-                disabled={currentRepoIndex === 0}
-                onClick={() => {
-                  setCurrentRepoIndex(currentRepoIndex - 1);
-                }}
-              >
-                Previous
-              </Button>
-              <Repo repo={repos[currentRepoIndex]} />
-              <Button
-                color="light-blue"
-                disabled={currentRepoIndex === repos.length - 1}
-                onClick={() => {
-                  setCurrentRepoIndex(currentRepoIndex + 1);
-                }}
-              >
-                Next
-              </Button>
-            </div>
-          </div>
-        </div>
+  useEffect(() => {
+    async function fetchData() {
+      const stats = await getRepositoryStats(repos[currentRepoIndex]);
+      setRepoStats(stats);
+    }
+    fetchData();
+  }, [currentRepoIndex]);
+
+  function handleNextRepo() {
+    if (currentRepoIndex < repos.length - 1) {
+      setCurrentRepoIndex(currentRepoIndex + 1);
+    }
+  }
+
+  function handlePrevRepo() {
+    if (currentRepoIndex > 0) {
+      setCurrentRepoIndex(currentRepoIndex - 1);
+    }
+  }
+
+  const repoImagePosition = currentRepoIndex % 2 === 0 ? "left" : "right";
+
+  return (
+    <div className="flex flex-col items-center space-y-5">
+      <h1 className="text-3xl font-bold underline">Projects I've worked on</h1>
+      {repoStats ? (
+        <RepoCard stats={repoStats} image={repoImages[repoStats.repo]} imagePosition={repoImagePosition} />
+      ) : (
+        <p>Loading...</p>
+      )}
+      <div className="flex justify-between w-1/2">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          disabled={currentRepoIndex === 0}
+          onClick={handlePrevRepo}
+        >
+          Previous
+        </button>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          disabled={currentRepoIndex === repos.length - 1}
+          onClick={handleNextRepo}
+        >
+          Next
+        </button>
       </div>
+    </div>
   );
 }
-
 export default Github;
