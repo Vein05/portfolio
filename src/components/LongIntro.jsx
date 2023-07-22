30fbae8bc0664a746ead10897e87b4626624bd9a
Vein05 <72024472+Vein05@users.noreply.github.com>
2023-07-22 14:59:34 +0545

    Changed how the quotes were working

--- src/components/LongIntro.jsx
+++ src/components/LongIntro.jsx
@@ -4,17 +4,13 @@ import Table from "./Table"
 function LongIntro(){
     return (
         <div id="Qualifications">
-            <div className="flex items-center space-x-3 overflow-hidden box-border pl-5">
-                <img src={Image} alt="myPhoto" className= "w-1/2 rounded-lg" />
-
-                <p className="pl-2  font-ClashGrotesk-Light">
-                <div class="text-2xl text-gray-600 inline-flex">“</div>
-                Endowed with a passion for technology, I embark on a quest to unravel the enigmatic mysteries of the digital realm. 
-                Through relentless pursuit of knowledge and innovation, I push the limits of what's possible and explore uncharted territories,
-                eager to discover endless possibilities.
-                <div className="h-3 text-2xl inline-flex text-gray-600">”</div>
-                </p>
-                
+            <div className="flex flex-col lg:flex lg:flex-row items-center space-x-3 pl-5">
+                <img src={Image} alt="myPhoto" className= "h-48 lg:h-96 rounded-lg" />
+                <span className="text-xl pl-2">
+                    “Endowed with a passion for technology, I embark on a quest to unravel the enigmatic mysteries of the digital realm. 
+                    Through relentless pursuit of knowledge and innovation, I push the limits of what's possible and explore uncharted territories,
+                    eager to discover endless possibilities.”
+                </span>
 
             </div>
             <div>
