9bafa5b4a9bc3a18ad6805528e458beabd5e2875
Vein05 <72024472+Vein05@users.noreply.github.com>
2023-11-18 11:23:49 +0545

    Small tweak

--- src/pages/Qin/Letter.jsx
+++ src/pages/Qin/Letter.jsx
@@ -1,7 +1,7 @@
 export default function Letter(){
     return(
-        <div class="flex ietms-center justify-center">
-            <span class="center"> I love you.... More coming soon!!</span>
+        <div class="m-10 flex ietms-center justify-center">
+            <span class="text-center"> I love you.... More coming soon!!</span>
         </div>
     )
 } 
