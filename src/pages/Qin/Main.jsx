4c1a039a734c225ed1593ffe4845c956c43cb093
Vein05 <man359905@gmail.com>
2024-06-21 08:44:34 +0545

    Changed the initial layout and added a password system with env

--- src/pages/Qin/Main.jsx
+++ src/pages/Qin/Main.jsx
@@ -2,44 +2,46 @@ import React, {useState} from "react";
 import AfterLogin from "./AfterLogin";
 
 function Main() {
-  const [password, setPassword] = useState("");
-  const [isValid, setIsValid] = useState(null);
-  const correctPassword = process.env.REACT_APP_QIN_PASS
+    const [password, setPassword] = useState("");
+    const [isValid, setIsValid] = useState(null);
+    const correctPassword = process.env.REACT_APP_QIN_PASS
 
-  function handlePasswordChange(event) {
-    setPassword(event.target.value);
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    if (password === correctPassword) {
-      setIsValid(true);
-    } else {
-      setIsValid(false);
+    function handlePasswordChange(event) {
+        setPassword(event.target.value);
     }
-  }
 
-  return (
-    <div className="h-full space-x-5 bg-blue-100 flex flex-col items-center justify-center">
-      <p>Please enter the password:</p>
-      <form onSubmit={handleSubmit}>
-        <input
-          id="pwd"
-          type="password"
-          className="rounded-md text-center"
-          value={password}
-          onChange={handlePasswordChange}
-        />
-        <input
-          className="ml-3 p-0.5 rounded-md bg-blue-300"
-          type="submit"
-          value="Submit"
-        />
-      </form>
-      {isValid === false && <div className="mt-3 text-red-500">Wrong password, please try again.</div>}
-      {isValid === true && <AfterLogin/>}
-    </div>
-  );
+    function handleSubmit(event) {
+        event.preventDefault();
+        if (password === correctPassword) {
+            setIsValid(true);
+        } else {
+            setIsValid(false);
+        }
+    }
+
+    return (
+        <div className="flex items-center justify-center ">
+            <div>
+                <p>Please enter the password:</p>
+                <form onSubmit={handleSubmit}>
+                    <input
+                        id="pwd"
+                        type="password"
+                        className="rounded-md text-center"
+                        value={password}
+                        onChange={handlePasswordChange}
+                    />
+                    <input
+                        className="ml-3 p-0.5 border-black rounded-md bg-blue-300"
+                        type="submit"
+                        value="Submit"
+                    />
+                </form>
+                {isValid === false && <div className="mt-3 text-red-500">Wrong password, please try again.</div>}
+                {isValid === true && <AfterLogin/>}
+            </div>
+        </div>
+    );
 }
 
 export default Main;
