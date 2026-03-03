63f86d32d8179fda2e6bfafe3c77a0379eace175
Sugam Panthi <man359905@gmail.com>
2026-03-02 23:50:07 -0600

    chore(routing): remove AcademicQualifications page and route

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Home from "./pages/Home"
 import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NoPage from "./pages/NoPage"
-import AcademicQualifications from "./pages/AcademicQualtifications"
 import BlogDirectory from "./pages/blog/BlogDirectory"
 import BlogPost from "./pages/blog/BlogPost"
 function App() {
@@ -14,7 +13,6 @@ function App() {
         <Routes>
           <Route index element={<Home />} />
           <Route path="/home" element={<Home />} />
-          <Route path="/pages/academic-qualifications" element={<AcademicQualifications />} />
           <Route path="/blog" element={<BlogDirectory />} />
           <Route path="/blog/:slug" element={<BlogPost />} />
 
